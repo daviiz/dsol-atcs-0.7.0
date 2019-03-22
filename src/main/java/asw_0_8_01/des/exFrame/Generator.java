b8215902584a4bad7c09fd4f3351254342d4874a
WZ.Dai <daviiz1117@163.com>
2019-03-22 16:41:36 +0800

    update biz code

--- src/main/java/asw_0_8_01/des/exFrame/Generator.java
+++ src/main/java/asw_0_8_01/des/exFrame/Generator.java
@@ -8,19 +8,33 @@ import devs.core.AtomicModelBase;
 import devs.core.InputPortBase;
 import devs.core.OutputPortBase;
 import nl.tudelft.simulation.dsol.formalisms.devs.ESDEVS.CoupledModel;
+import nl.tudelft.simulation.dsol.formalisms.devs.ESDEVS.Phase;
 import nl.tudelft.simulation.dsol.simulators.DEVSSimulator;
 
 public class Generator extends AtomicModelBase<GeneratorOm> {
 
 
     public InputPortBase<wp_launch> in_wp_launch;
+    public InputPortBase<Boolean> in_engage_result;
     public OutputPortBase<scen_info> out_scen_info;
     public OutputPortBase<entity_info> out_entity_info;
 
+    private wp_launch in_wp_launch_value;
+    private boolean in_engage_result_value;
+    private scen_info out_scen_info_value;
+    private entity_info out_entity_info_value;
+
+    private boolean isFireWeapon = false;
+    private boolean isReplicaiton = false;
+
+    private Phase WAIT,GEN;
+
+    private int replicationCount = 0;
+
     @Override
     protected void constructPort() {
-
         in_wp_launch = new InputPortBase<wp_launch>(this);
+        in_engage_result = new InputPortBase<Boolean>(this);
         out_entity_info = new OutputPortBase<entity_info>(this);
         out_scen_info = new OutputPortBase<scen_info>(this);
     }
@@ -32,22 +46,58 @@ public class Generator extends AtomicModelBase<GeneratorOm> {
 
     @Override
     protected void constructPhase() {
-
+        WAIT = new Phase("WAIT"); WAIT.setLifeTime(Double.POSITIVE_INFINITY);
+        GEN = new Phase("GEN"); WAIT.setLifeTime(0.0);
+        this.phase = GEN;
     }
 
     @Override
-    protected void deltaExternalFunc(Object value) {
+    protected void deltaExternalFunc(Object value){
+        if(this.phase.getName().equals(WAIT.getName())){
+            if(this.activePort == in_wp_launch){
+                /**
+                 *to-do:发射武器：鱼雷/鱼雷诱饵，从wp_launch中获取武器实体的初始位置
+                 */
+                in_wp_launch_value = (wp_launch)value;
 
+                isFireWeapon = true;
+            }
+            if(this.activePort == in_engage_result && replicationCount<100){
+                /**
+                 * to-do:执行下一个Replication：
+                 */
+                in_engage_result_value = (Boolean)value;
+                System.out.println("Rep_"+replicationCount+",已经执行完成了，是否成功拦截了鱼雷？ "+(in_engage_result_value ? "是": "否"));
+                isReplicaiton = true;
+            }
+            this.phase  = GEN;
+        }
     }
 
     @Override
     protected void deltaInternalFunc() {
+        if(this.phase.getName().equals(GEN.getName())){
+            //执行第一个Replication：
 
+            replicationCount += 1;
+            this.phase  = WAIT;
+        }
     }
 
     @Override
     protected void lambdaFunc() {
-
+        if(this.phase.getName().equals(GEN.getName())){
+            if(isFireWeapon){
+                out_entity_info_value.setSenderId("Rep_"+replicationCount);
+                out_entity_info.send(out_entity_info_value);
+                isFireWeapon = false;
+            }
+            if(isReplicaiton){
+                out_scen_info_value.setSenderId("Rep_"+replicationCount);
+                out_scen_info.send(out_scen_info_value);
+                isFireWeapon = false;
+            }
+        }
     }
 
     public Generator(String modelName, CoupledModel.TimeDouble parentModel) {
--- src/main/java/asw_0_8_01/des/exFrame/Transducer.java
+++ src/main/java/asw_0_8_01/des/exFrame/Transducer.java
@@ -4,10 +4,17 @@ import asw_0_8_01.om.TransducerOm;
 import devs.core.AtomicModelBase;
 import devs.core.InputPortBase;
 import nl.tudelft.simulation.dsol.formalisms.devs.ESDEVS.CoupledModel;
+import nl.tudelft.simulation.dsol.formalisms.devs.ESDEVS.Phase;
 import nl.tudelft.simulation.dsol.simulators.DEVSSimulator;
 
 public class Transducer extends AtomicModelBase<TransducerOm> {
     public InputPortBase<Boolean> in_engage_result;
+
+    private int replicationCount = 0;
+
+    private int survivalCount = 0;
+
+    private Phase WAIT,STAT;
 
     @Override
     protected void constructPort() {
@@ -21,17 +28,33 @@ public class Transducer extends AtomicModelBase<TransducerOm> {
 
     @Override
     protected void constructPhase() {
-
+        WAIT = new Phase("WAIT"); WAIT.setLifeTime(Double.POSITIVE_INFINITY);
+        STAT = new Phase("STAT"); WAIT.setLifeTime(0.0);
+        this.phase = STAT;
     }
 
     @Override
     protected void deltaExternalFunc(Object value) {
-
+        if(this.phase.getName().equals(WAIT.getName())){
+            Boolean tmp = (Boolean)value;
+            replicationCount += 1;
+            if(tmp){
+                survivalCount +=1;
+            }
+            this.phase = STAT;
+        }
     }
 
     @Override
     protected void deltaInternalFunc() {
-
+        if(this.phase.getName().equals(STAT.getName()) && replicationCount > 0 ){
+            /**
+             * 统计拦截效率：
+             */
+            System.out.println("当前样本执行了："+replicationCount+" 次；"+"成功拦截:"+survivalCount+" 次；"+"拦截效率为："+(survivalCount/replicationCount));
+            replicationCount += 1;
+            this.phase  = WAIT;
+        }
     }
 
     @Override
